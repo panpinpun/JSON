984aa350df582f154a822181f10a53b6860aaf87
panpinpun <48430463+panpinpun@users.noreply.github.com>
2019-03-22 15:30:47 +0700

    Add files via upload

---
+++ konfigurasi_database.php
@@ -0,0 +1,8 @@
+<?php 
+$host = "localhost";
+$user = "id8194880_rstr1";
+$pass = "12345678";
+$db = "id8194880_restoran";
+ 
+$conn = mysqli_connect($host,$user,$pass,$db) or die("Error" . mysqli_error($conn));
+?>
